e87981d02f73c9695ca14328abd573179a727374
jtc42 <jtc9242@gmail.com>
2017-08-17 23:03:21 +0100

    Catching for OHM unavailable (prevents borked JSON return)

--- tools/sysinfo.py
+++ tools/sysinfo.py
@@ -13,14 +13,19 @@ w = wmi.WMI(namespace="root\OpenHardwareMonitor")
 sensors = [('CPU Total', 'Load'), ('CPU Package', 'Temperature'), ('GPU Core', 'Load'), ('GPU Core', 'Temperature'), ('Memory', 'Load'), ('GPU Memory', 'Load')]
 
 def get_all(s_list):
-    # Get sensor data. Note: This totally rebuilds the sensor array, so targetting below is required at each run
-    data_all = w.Sensor()  
-    
     data = {}
     
-    for d in data_all:
+    # Get sensor data. Note: This totally rebuilds the sensor array, so targetting below is required at each run
+    data_all = w.Sensor()
+    if len(data_all) > 0:
+        for d in data_all:
+            for s in s_list:
+                if d.Name == s[0] and d.SensorType == s[1]:
+                    data["{0}/{1}".format(d.Name, d.SensorType)] = round(d.Value,2)
+    else:
+        print("OpenHardwareMonitor cannot be found.")
         for s in s_list:
-            if d.Name == s[0] and d.SensorType == s[1]:
-                data["{0}/{1}".format(d.Name, d.SensorType)] = round(d.Value,2)
+            data["{0}/{1}".format(s[0], s[1])] = 0
     
+    print(data)
     return data
